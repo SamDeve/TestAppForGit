0d57fe9381d1e2e1db85ca020f7ac5f84ccfcede
Manuj Sarpal <ManujSarpal@hotmail.com>
2018-06-03 19:58:10 +0100

    added tests

---
+++ TestAppForGit/Maths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppForGit
+{
+    public class Maths
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first");
+        }
+    }
+}
